8783d38dc35c487541dc5cd8fe66eeff6bccff4d
yared aregayehu <yaredbaby21@gmail.com>
2026-04-17 15:30:14 -0500

    `Added various controllers, models, middleware, and routes for user, course, quiz, and progress management`

---
+++ backend/config/jwt.js
@@ -0,0 +1,11 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (userId, role) => {
+  return jwt.sign({ id: userId, role }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+};
+
+export const verifyToken = (token) => {
+  return jwt.verify(token, process.env.JWT_SECRET);
+};
